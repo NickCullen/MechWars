b9bee50115e771c0d19d2bc932410a1563698960
Nick Cullen <nmcullen91@gmail.com>
2016-03-30 22:28:01 +0100

    inital commit

---
+++ Source/MechWars/MechWars.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MechWars.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MechWars, "MechWars" );
